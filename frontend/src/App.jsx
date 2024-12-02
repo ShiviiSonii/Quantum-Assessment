13ff61e035bf4c9457893c1ba0a6936528922f14
ShiviiSonii <1111dhruvsoni@gmail.com>
2024-12-03 00:09:25 +0530

    updated protected page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,29 @@
-import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import Register from "./pages/Register.jsx"
-import Login from "./pages/Login.jsx"
-import Protected from "./pages/Protected.jsx"
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Register from './pages/Register.jsx';
+import Login from './pages/Login.jsx';
+import Protected from './pages/Protected.jsx';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsAuthenticated(!!token); 
+  }, []);
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Register/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/protected' element={<Protected/>}/>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route
+          path='/protected'
+          element={isAuthenticated ? <Protected /> : <Navigate to='/login' />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
